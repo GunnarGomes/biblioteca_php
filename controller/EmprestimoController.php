33ebf3345cce6ad72b06a3f3c49ce8df05273c3e
GUNNAR FORNAZIER GOMES <00001099296365sp@aluno.educacao.sp.gov.br>
2025-06-26 11:00:25 -0300

    1

--- controller/EmprestimoController.php
+++ controller/EmprestimoController.php
@@ -11,7 +11,30 @@ class EmprestimoController
         $this->emprestimoModel = new Emprestimo($db);
     }
 
-    
+    function showEmprestimoCad()
+    {
+
+    }
+
+    function cadastrarEmprestimo()
+    { 
+        
+        $idLivro = $_POST["livro"];
+        $idAluno = $_POST["aluno"];
+        $idProfessor = $_POST["professor"];
+        $dataEmprestimo = $_POST["data_emprestimo"];
+        $dataDevolucao = $_POST["data_devolucao"];
+
+        $res = $this->emprestimoModel->emprestar($idLivro,$idAluno,$idProfessor,$dataEmprestimo,$dataDevolucao);
+
+        if ($res === true)
+        {
+            echo "emprestimo feito com sucesso";
+        } else {
+            echo "algo deu errado<br>";
+            echo $res;
+        }
+    }
 
 }
 
